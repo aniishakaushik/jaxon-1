5ceed9ba93a50b0cb1169b50c41d53bfa0e77dac
aniishakaushik <84014671+aniishakaushik@users.noreply.github.com>
2021-07-20 09:04:12 +0530

    Add files via upload

---
+++ jaxon/sketch.js
@@ -0,0 +1,18 @@
+
+function preload(){
+  //pre-load images
+}
+pathImg.loadImage=(path.png)
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+}
+// moving background
+path=createsprite=(200,200)
+path.addImage(path.png)
+path.velocityY=4;
+function draw() {
+
+  background(0);
+
+}
